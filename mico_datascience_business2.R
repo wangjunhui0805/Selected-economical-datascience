6f55e49b1743f895beb401114be5d25699c78a6f
wangjunhui0805 <wangjunhui0805@gmail.com>
2025-02-10 15:30:04 +0900

    Update mico_datascience_business2.R
    グラフの加工、x軸とy軸のラベルを変えた。
    -データの分布を見分けるために、点を透明にした。

--- mico_datascience_business2.R
+++ mico_datascience_business2.R
@@ -20,19 +20,30 @@ store_test <- left_join(store_1234,store,by=c("Store"))
 summary(store_test)
 
 
+
 ## 1変数
 　# sales_open_histogram
 
 
-plot_sales<- ggplot(store_1234, aes(Sales)) + geom_histogram()
+plot_sales<- ggplot(store_1234, aes(Sales)) + geom_histogram() + 
+  labs(title = "店舗の売り上げ分布",x = "売り上げ", y = "頻度") +  
+  theme(text = element_text(family = "Hiragino Sans")) 
 print(plot_sales)
 
 store_open <- filter(store_1234, Open == 1)
-plot_sales_open<- ggplot(store_open, aes(Sales)) + geom_histogram()
+plot_sales_open<- ggplot(store_open, aes(Sales)) + 
+  geom_histogram() +
+  labs(title = "店舗の売り上げ分布（営業日のみ）",x = "売り上げ", y = "頻度") +  
+  theme(text = element_text(family = "Hiragino Sans")) 
+
 print(plot_sales_open)
 
 　#sales_open_kernel(density)
-plot_sales_open<- ggplot(store_open, aes(Sales)) + geom_density(kernel = "gaussian",bw = 1000.0)
+plot_sales_open<- ggplot(store_open, aes(Sales)) + 
+  geom_density(kernel = "gaussian",bw = 1000.0) +
+  labs(title = "店舗の売り上げ分布（営業日のみ）",x = "売り上げ", y = "頻度") +  
+  theme(text = element_text(family = "Hiragino Sans")) 
+
 print(plot_sales_open)
 　#plot_sales_open<- ggplot(store_open, aes(Sales)) + geom_density(kernel = "gaussian",adjust = 1.0)
 　#print(plot_sales_open)
@@ -45,10 +56,17 @@ summary(store_open$Sales)
 
 　#customers hist
 library(tidyverse)
-plot_customers<- ggplot(store_open, aes(Customers)) + geom_histogram()
+plot_customers<- ggplot(store_open, aes(Customers)) + 
+  geom_histogram() +
+  labs(title = "顧客数分布（営業日のみ）",x = "顧客数", y = "頻度") +  
+  theme(text = element_text(family = "Hiragino Sans")) 
+
 print(plot_customers)
 plot_customers_open<- ggplot(store_open, aes(Customers)) + 
-  geom_density(kernel = "gaussian",bw = 1000.0)
+  geom_density(kernel = "gaussian",bw = 1000.0) +
+  labs(title = "顧客数分布（営業日のみ）",x = "顧客数", y = "頻度") +  
+  theme(text = element_text(family = "Hiragino Sans")) 
+
 print(plot_customers_open)
 　#記述
 summary(store_open$Customers)
@@ -63,9 +81,13 @@ summary(store_open$Customers)
 
 ##2変量
  #geom_pointで　sale and customers 回帰分析
-sales_customers <- ggplot(store_open, aes( x = Customers, y = Sales)) + geom_point() +
+sales_customers <- ggplot(store_open, aes( x = Customers, y = Sales)) + 
+  geom_point(shape = 21) +
   geom_smooth(method = "lm", formula = y ~ x , 
-                            color = "red", se = FALSE)
+                            color = "red", se = FALSE) +
+  labs(title = "顧客数と売上の相関分析",x = "顧客数", y = "売上") +  
+  theme(text = element_text(family = "Hiragino Sans")) 
+
 print(sales_customers)
 
 model_sc <- lm(Sales ~ Customers, data = store_open)
@@ -73,18 +95,30 @@ summary(model_sc)
 
 
 #geom_boxplotで　sale and promo
-sales_promo_box <- ggplot(store_open, aes(factor(Promo),Sales)) + geom_boxplot()
+sales_promo_box <- ggplot(store_open, aes(factor(Promo),Sales)) + 
+  geom_boxplot() +
+  labs(title = "売上とプロモーションの相関関係",x = "プロモーションの状態", y = "売上") +  
+  theme(text = element_text(family = "Hiragino Sans")) 
+
 print(sales_promo_box)
 
 #geom_boxplotで　customers and holiday
-holiday_customers_box <- ggplot(store_open, aes(factor(StateHoliday),Customers)) + geom_boxplot()
+holiday_customers_box <- ggplot(store_open, aes(factor(StateHoliday),Customers)) + 
+  geom_boxplot() +
+  labs(title = "祝日と顧客数の関係",x = "祝日", y = "顧客数") +  
+  theme(text = element_text(family = "Hiragino Sans")) 
+
 print(holiday_customers_box)
 
 #geom_boxplotで　customers and school holiday
 schoolholiday_customers_box <- ggplot(store_open, aes(factor(SchoolHoliday),Customers)) + geom_boxplot()
 print(schoolholiday_customers_box)
 #geom_boxplotで　store_type and sales
-storetype_sales <- ggplot(store_test, aes(factor(StoreType),Sales)) + geom_boxplot()
+storetype_sales <- ggplot(store_test, aes(factor(StoreType),Sales)) + 
+  geom_boxplot() +
+  labs(title = "店舗の種類ごとに売上",x = "店舗種類", y = "売り上げ") +  
+  theme(text = element_text(family = "Hiragino Sans")) 
+
 print(storetype_sales)
 
 #回帰分析
@@ -97,7 +131,10 @@ summary(model_csh)
 resid_csh <- resid(model_csh)
 csh_point_resid <- ggplot(store_open, 
                           aes(StateHoliday, resid_csh)) + 
-  geom_point()
+  geom_point(shape = 21) +
+  labs(title = "祝日と顧客数モデルの残差分布",x = "祝日", y = "予測した顧客数の残差") +  
+  theme(text = element_text(family = "Hiragino Sans")) 
+
 print(csh_point_resid)
 #ダミー変数の場合、回帰線が書けない、箱ひげ図に変わる
   # geom_smooth(method = "lm", formula = y ~ x , 
@@ -136,20 +173,22 @@ stargazer(model_sp, model_sps, model_spsc, type = "text",
                     title = "sales_promo")
 
 #sales_cpdist
-# 競合店がすべて営業中であることを確認するために、開店日の日付形式を変換する
+# 将年和月转换为日期
 store_test$CompetitionMonYEAR <- 
   as.Date(paste(store_test$CompetitionOpenSinceYear, 
                 store_test$CompetitionOpenSinceMonth, "01", sep = "-"))
 
-# 転換した結果を確認する
+# 查看结果
 print(store_test$CompetitionMonYEAR)
-# 競合店がすべて営業中であることを確保する
-sales_cpdist <- filter(store_test, Date >= CompetitionMonYEAR)
 
+sales_cpdist <- filter(store_test, Date >= CompetitionMonYEAR)
 sales_cpdist_point <- ggplot(sales_cpdist, aes( x = CompetitionDistance, y = Sales)) + 
-  geom_point() +
+  geom_point(shape = 21) +
   geom_smooth(method = "lm", formula = y ~ x , 
-              color = "red", se = FALSE)
+              color = "red", se = FALSE) +
+  labs(title = "競合店との距離と売上の相関分析",x = "競合店との距離", y = "売上") +  
+  theme(text = element_text(family = "Hiragino Sans")) 
+
 print(sales_cpdist_point)
 
 model_sales_cpdist <- lm(Sales ~ CompetitionDistance, 
@@ -162,7 +201,10 @@ mean_store <- store_open %>%
   group_by(Date) %>%
   summarise(Average_sell = mean(Sales))
 timeseriese_daymean<-ggplot(mean_store, aes(x=Date,y=Average_sell))+
-  geom_line()
+  geom_line() +
+  labs(title = "日平均の売り上げの時系列",x = "時間", y = "日ごとの平均売上") +  
+  theme(text = element_text(family = "Hiragino Sans")) 
+
 print(timeseriese_daymean)
 
 #時系列の図 store1の変化
@@ -170,7 +212,10 @@ store1_sales <- filter(store_open,Store == 1)
 library(ggplot2)
 timeseriese<-ggplot(store1_sales, aes(x=Date,y=Sales))+
   geom_line()+
-  facet_wrap(~ Store, scales="free_y")
+  facet_wrap(~ Store, scales="free_y") +
+  labs(title = "個別売り上げの時系列",x = "時間", y = "store1 日ごとの売上") +  
+  theme(text = element_text(family = "Hiragino Sans")) 
+
 print(timeseriese)
 
 
